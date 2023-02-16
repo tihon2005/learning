358e5bf38445620785b9c834110e6e8dc9e0d3cc
Tsikhon Bazar <103754328+tihon2005@users.noreply.github.com>
2023-02-16 12:47:45 +0300

    Add files via upload

--- SelectionApp/SelectionApp/ViewController.swift
+++ SelectionApp/SelectionApp/ViewController.swift
@@ -14,12 +14,15 @@ class ViewController: UICollectionViewController {
         // Do any additional setup after loading the view.
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        return 102
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
         cell.label.text = String(indexPath.row + 1)
-        if (indexPath.row + 1)%3 == 0{
+        if (indexPath.row + 1)%6 == 0{
+            cell.backgroundColor = .yellow
+        }
+        else if (indexPath.row + 1)%3 == 0{
             cell.backgroundColor = .blue
         }
         else if (indexPath.row + 1)%2 == 0{
